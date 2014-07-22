ba0d7f664c29d02460676ca35e113a65f97e895e
Pēteris Caune <cuu508@gmail.com>
2014-07-22 14:25:52 +0300

    Hardcoding configuration.

--- pilbox/app.py
+++ pilbox/app.py
@@ -30,13 +30,6 @@ from tornado.options import define, options, parse_config_file
 
 from pilbox import errors
 from pilbox.image import Image
-from pilbox.signature import verify_signature
-
-try:
-    from urlparse import urlparse, urljoin
-except ImportError:
-    from urllib.parse import urlparse, urljoin
-
 
 # general settings
 define("config", help="path to configuration file",
@@ -44,38 +37,18 @@ define("config", help="path to configuration file",
 define("debug", help="run in debug mode", type=bool, default=False)
 define("port", help="run on the given port", type=int, default=8888)
 
-# security related settings
-define("client_name", help="client name")
-define("client_key", help="client key")
-define("allowed_hosts", help="list of valid hosts", default=[], multiple=True)
-
 # request related settings
 define("max_requests", help="max concurrent requests", type=int, default=40)
 define("timeout", help="request timeout in seconds", type=float, default=10)
 define("implicit_base_url", help="prepend protocol/host to url paths")
 define("validate_cert", help="validate certificates", type=bool, default=True)
 
-# default image option settings
-define("filter", help="default filter to use when resizing")
-define("format", help="default format to use when outputting")
-define("optimize", help="default to optimize when saving", type=int)
-define("quality", help="default jpeg quality, 0-100", type=int)
-
 logger = logging.getLogger("tornado.application")
-
 
 class PilboxApplication(tornado.web.Application):
 
     def __init__(self, **kwargs):
         settings = dict(debug=options.debug,
-                        client_name=options.client_name,
-                        client_key=options.client_key,
-                        allowed_hosts=options.allowed_hosts,
-                        filter=options.filter,
-                        format=options.format,
-                        mode=options.mode,
-                        optimize=options.optimize,
-                        quality=options.quality,
                         max_requests=options.max_requests,
                         timeout=options.timeout,
                         implicit_base_url=options.implicit_base_url,
@@ -84,20 +57,10 @@ class PilboxApplication(tornado.web.Application):
         tornado.web.Application.__init__(self, self.get_handlers(), **settings)
 
     def get_handlers(self):
-        return [(r"/", ImageHandler, dict(w=100, h=100))]
+        return [(r"/", ImageHandler, dict(w=200, h=200))]
 
 
 class ImageHandler(tornado.web.RequestHandler):
-    FORWARD_HEADERS = ['Cache-Control', 'Expires', 'Last-Modified']
-    OPERATIONS = ["region", "resize", "rotate", "noop"]
-
-    _FORMAT_TO_MIME = {
-        "gif": "image/gif",
-        "jpeg": "image/jpeg",
-        "jpg": "image/jpeg",
-        "png": "image/png",
-        "webp": "image/webp"}
-
     w = None
     h = None
 
@@ -122,7 +85,8 @@ class ImageHandler(tornado.web.RequestHandler):
             raise errors.FetchError()
 
         outfile = self._process_response(resp)
-        self._forward_headers(resp.headers)
+        self._set_headers()
+
         for block in iter(lambda: outfile.read(65536), b""):
             self.write(block)
         outfile.close()
@@ -146,17 +110,12 @@ class ImageHandler(tornado.web.RequestHandler):
     def _process_response(self, resp):
         image = Image(resp.buffer)
 
-        image.resize(self.w, self.h, **self.settings)
-        return image.save(**self.settings)
+        image.resize(self.w, self.h)
+        return image.save()
 
-    def _forward_headers(self, headers):
-        mime = self._FORMAT_TO_MIME.get(
-            self.get_argument("fmt", self.settings.get("format")),
-            headers['Content-Type'])
-        self.set_header('Content-Type', mime)
-        for k in ImageHandler.FORWARD_HEADERS:
-            if k in headers and headers[k]:
-                self.set_header(k, headers[k])
+    def _set_headers(self):
+        self.set_header('Content-Type', "image/jpeg")
+        self.set_header('Cache-Control', "public, max-age=31536000") # 1 year
 
 def main():
     tornado.options.parse_command_line()
--- pilbox/image.py
+++ pilbox/image.py
@@ -22,7 +22,6 @@ import re
 import os.path
 
 import PIL.Image
-import PIL.ImageOps
 
 from pilbox import errors
 
@@ -33,29 +32,8 @@ except ImportError:
 
 logger = logging.getLogger("tornado.application")
 
-_filters_to_pil = {
-    "antialias": PIL.Image.ANTIALIAS,
-    "bicubic": PIL.Image.BICUBIC,
-    "bilinear": PIL.Image.BILINEAR,
-    "nearest": PIL.Image.NEAREST
-    }
-
-_formats_to_pil = {
-    "gif": "GIF",
-    "jpg": "JPEG",
-    "jpeg": "JPEG",
-    "png": "PNG",
-    "webp": "WEBP"
-}
-
-
 class Image(object):
-    FILTERS = _filters_to_pil.keys()
-    FORMATS = _formats_to_pil.keys()
-    MODES = ["clip", "crop", "fill", "scale"]
-
-    _DEFAULTS = dict(background="fff", expand=False, filter="antialias",
-                     format=None, mode="crop", optimize=False, quality=90)
+    FORMATS = ("gif", "jpg", "jpeg", "png", "webp")
 
     def __init__(self, stream):
         self.stream = stream
@@ -64,44 +42,26 @@ class Image(object):
         if self.img.format.lower() not in self.FORMATS:
             raise errors.ImageFormatError(
                 "Unknown format: %s" % self.img.format)
-        self._orig_format = self.img.format
 
-    def resize(self, width, height, **kwargs):
+    def resize(self, width, height):
         """Resizes the image to the supplied width/height. Returns the
-        instance. Supports the following optional keyword arguments:
+        instance. """
 
-        filter - The filter to use: see Image.FILTERS
-        """
-        opts = Image._normalize_options(kwargs)
         size = self._get_size(width, height)
-        self._clip(size, opts)
+        self._clip(size)
         return self
 
+    def save(self):
+        """Returns a buffer to the image for saving. """
 
-    def save(self, **kwargs):
-        """Returns a buffer to the image for saving, supports the
-        following optional keyword arguments:
-
-        format - The format to save as: see Image.FORMATS
-        optimize - The image file size should be optimized
-        quality - The quality used to save JPEGs: integer from 1 - 100
-        """
-        opts = Image._normalize_options(kwargs)
         outfile = BytesIO()
-        if opts["pil"]["format"]:
-            fmt = opts["pil"]["format"]
-        else:
-            fmt = self._orig_format
-        save_kwargs = dict(quality=int(opts["quality"]))
-        if int(opts["optimize"]):
-            save_kwargs["optimize"] = True
-        self.img.save(outfile, fmt, **save_kwargs)
+        self.img.save(outfile, "JPEG", quality=85)
         outfile.seek(0)
 
         return outfile
 
-    def _clip(self, size, opts):
-        self.img.thumbnail(size, opts["pil"]["filter"])
+    def _clip(self, size):
+        self.img.thumbnail(size, PIL.Image.ANTIALIAS)
 
     def _get_size(self, width, height):
         aspect_ratio = self.img.size[0] / self.img.size[1]
@@ -111,18 +71,3 @@ class Image(object):
             height = int((int(width) or self.img.size[0]) / aspect_ratio)
         return (int(width), int(height))
 
-    @staticmethod
-    def _normalize_options(options):
-        opts = Image._DEFAULTS.copy()
-        for k, v in options.items():
-            if v is not None:
-                opts[k] = v
-        opts["pil"] = dict(
-            filter=_filters_to_pil.get(opts["filter"]),
-            format=_formats_to_pil.get(opts["format"]))
-
-        if not opts["pil"]["position"]:
-            opts["pil"]["position"] = _positions_to_ratios.get(
-                opts["position"], None)
-
-        return opts
